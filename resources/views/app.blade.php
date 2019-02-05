da38e8748d07b4702e3881fca8d8e525ebd5e48d
xy2z <xyzbk89@gmail.com>
2019-02-05 21:07:26 +0100

    Layout - Work in progress

---
+++ resources/views/app.blade.php
@@ -0,0 +1,119 @@
+@extends('assets.layout')
+
+@section('content')
+	<div id="app">
+		<nav>
+			<a class="logo" href="/">dopenote</a>
+
+			<div class="nav-notes">
+				<a href="#">note 1</a>
+				<a href="#">note 2</a>
+				<a href="#" class="active">note 3</a>
+				<a href="#">note 4</a>
+				<a href="#">note 5</a>
+				<a href="#">note 6</a>
+				<a href="#">note 7</a>
+				<a href="#">note 8</a>
+			</div>
+		</nav>
+
+		<div id="note">
+			<div class="note-title">
+				note 3
+			</div>
+			<div class="note-content">
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+				Content goes here.<br />
+			</div>
+		</div>
+
+	</div>
+@endsection
---
+++ resources/views/assets/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/layout.css" />
+        <link rel="stylesheet" href="/css/nav.css" />
+        <link rel="stylesheet" href="/css/editor.css" />
+
+        <title>{{ Config::get('app.name') }}</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
