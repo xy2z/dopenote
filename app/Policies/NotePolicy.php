0a6c21357778a64cc30e17865bf74c11f2603cce
xy2z <xyzbk89@gmail.com>
2019-04-01 20:45:08 +0200

    #3 Implemented policies for Note and Notebook (permissions for updating) 2/2

---
+++ app/Policies/NotePolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Note;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class NotePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Validate user can update/delete a note
+     *
+     * @param User $user User model
+     * @param Note $note Note model
+     *
+     * @return bool
+     */
+    public function update(User $user, Note $note) {
+        return $user->id === $note->user_id;
+    }
+}
---
+++ app/Policies/NotebookPolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Notebook;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class NotebookPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Validate user can update/delete a notebook
+     *
+     * @param User $user User model
+     * @param Notebook $notebook Notebook model
+     *
+     * @return bool
+     */
+    public function update(User $user, Notebook $notebook) {
+        return $user->id === $notebook->user_id;
+    }
+}
