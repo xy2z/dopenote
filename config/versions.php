ff692f8836cbfd7f09f9276eef413cfbbd0e178a
xy2z <xyzbk89@gmail.com>
2019-05-14 20:06:30 +0200

    Fixed whitespace

--- config/versions.php
+++ config/versions.php
@@ -2,9 +2,9 @@
 
 return [
 	'vue' => '2.6.6',
-    'vue_draggable' => '15.0.0',
-    'vue-simple-context-menu' => '3.1.3',
+	'vue_draggable' => '15.0.0',
+	'vue-simple-context-menu' => '3.1.3',
 
-    'jquery' => '3.4.1',
-    'sortable' => '1.8.3',
+	'jquery' => '3.4.1',
+	'sortable' => '1.8.3',
 ];
