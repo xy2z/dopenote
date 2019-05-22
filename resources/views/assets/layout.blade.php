0581f17940cda36c79ac4f6c6aeb5d48aecd0486
xy2z <xyzbk89@gmail.com>
2019-05-22 21:20:52 +0200

    #1 New favicon, matches logo

--- resources/views/assets/layout.blade.php
+++ resources/views/assets/layout.blade.php
@@ -4,7 +4,13 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="/icons/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#ff9900">
+        <meta name="msapplication-TileColor" content="#ff9900">
+        <meta name="theme-color" content="#ffffff">
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Carter+One" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css" />
