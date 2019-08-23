026dd27c95e1685293abb886048689090d21bbe4
xy2z <xyzbk89@gmail.com>
2019-08-23 13:55:58 +0200

    #97 Changed favicon so it's bigger and has less padding

--- resources/views/assets/layout.blade.php
+++ resources/views/assets/layout.blade.php
@@ -4,12 +4,15 @@
 		<meta charset="utf-8">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-		<link rel="manifest" href="/site.webmanifest">
-		<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#ff9900">
-		<meta name="msapplication-TileColor" content="#ff9900">
+		<link rel="manifest" href="/icons/site.webmanifest">
+		<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#f26222">
+		<link rel="shortcut icon" href="/icons/favicon.ico">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-config" content="/icons/browserconfig.xml">
 		<meta name="theme-color" content="#ffffff">
 
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Carter+One">
