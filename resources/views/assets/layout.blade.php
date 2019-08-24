c8340dcf67cff7d38a94ee0aa89012cd5281c7da
xy <xyzbk89@gmail.com>
2019-08-24 08:24:44 +0000

    Fixed manifest.json and browserconfig.xml

--- resources/views/assets/layout.blade.php
+++ resources/views/assets/layout.blade.php
@@ -5,10 +5,12 @@
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-capable" content="yes">
 		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-		<link rel="manifest" href="/img/site.webmanifest">
+		<link rel="manifest" href="/manifest.json">
 		<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#f26222">
 		<link rel="shortcut icon" href="/img/favicon.ico">
 		<meta name="msapplication-TileColor" content="#ffffff">
