ffa537e3416635574a20ca43ffb311ea2a5e0a54
xy <xyzbk89@gmail.com>
2019-08-24 08:04:21 +0000

    Moved icons to img/ dir (so it works)

--- resources/views/assets/layout.blade.php
+++ resources/views/assets/layout.blade.php
@@ -5,14 +5,14 @@
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-		<link rel="manifest" href="/icons/site.webmanifest">
-		<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#f26222">
-		<link rel="shortcut icon" href="/icons/favicon.ico">
+		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+		<link rel="manifest" href="/img/site.webmanifest">
+		<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#f26222">
+		<link rel="shortcut icon" href="/img/favicon.ico">
 		<meta name="msapplication-TileColor" content="#ffffff">
-		<meta name="msapplication-config" content="/icons/browserconfig.xml">
+		<meta name="msapplication-config" content="/img/browserconfig.xml">
 		<meta name="theme-color" content="#ffffff">
 
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Carter+One">
