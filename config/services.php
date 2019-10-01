ffddb647970e85736a542865cd21e930a6e5a515
junaid ahmad <junaid.ahmad@synavos.com>
2019-10-01 16:00:23 +0500

    formatting issues

--- config/services.php
+++ config/services.php
@@ -40,4 +40,22 @@ return [
         ],
     ],
 
+     'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/login/callback/facebook',
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/login/callback/google',
+    ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect' => env('APP_URL') . '/login/callback/twitter',
+    ],
+
 ];
