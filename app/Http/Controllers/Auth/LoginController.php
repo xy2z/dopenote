fcc94bef503da72dc00b88c04ef0e74f20265d45
junaid ahmad <junaid.ahmad@synavos.com>
2019-10-01 15:31:34 +0500

    formatting issues

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,89 +1,89 @@
-<?php
+	<?php
 
-namespace App\Http\Controllers\Auth;
+	namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\User;
-use Auth;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Socialite;
+	use App\Http\Controllers\Controller;
+	use App\User;
+	use Auth;
+	use Illuminate\Foundation\Auth\AuthenticatesUsers;
+	use Illuminate\Http\Request;
+	use Socialite;
 
-class LoginController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-     */
+	class LoginController extends Controller
+	{
+			/*
+			|--------------------------------------------------------------------------
+			| Login Controller
+			|--------------------------------------------------------------------------
+			|
+			| This controller handles authenticating users for the application and
+			| redirecting them to your home screen. The controller uses a trait
+			| to conveniently provide its functionality to your applications.
+			|
+			*/
 
-    use AuthenticatesUsers;
+			use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+			/**
+			 * Where to redirect users after login.
+			 *
+			 * @var string
+			 */
+			protected $redirectTo = '/';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+			/**
+			 * Create a new controller instance.
+			 *
+			 * @return void
+			 */
+			public function __construct()
+			{
+					$this->middleware('guest')->except('logout');
+			}
 
-    /**
-     * Redirect the user to the GitHub authentication page.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function redirectToProvider($service)
-    {
-        return Socialite::driver($service)->redirect();
-    }
+			/**
+			 * Redirect the user to the GitHub authentication page.
+			 *
+			 * @return \Illuminate\Http\Response
+			 */
+			public function redirectToProvider($service)
+			{
+					return Socialite::driver($service)->redirect();
+			}
 
-    /**
-     * Obtain the user information from GitHub.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function handleProviderCallback(Request $request, $service)
-    {
+			/**
+			 * Obtain the user information from GitHub.
+			 *
+			 * @return \Illuminate\Http\Response
+			 */
+			public function handleProviderCallback(Request $request, $service)
+			{
 
-        //retrieve user details
-        try {
-            $user = Socialite::driver($service)->user();
-        } catch (\Exception $e) {
-            return redirect('/login')->withErrors(['Please verify your .env keys for ' . $service . ' login.']);
-        }
+					//retrieve user details
+					try {
+							$user = Socialite::driver($service)->user();
+					} catch (\Exception $e) {
+							return redirect('/login')->withErrors(['Please verify your .env keys for ' . $service . ' login.']);
+					}
 
-        $socialAccountId = $user->getId();
-        $email = $user->getEmail();
-        $name = $user->getName();
-        $user = User::whereSocialAccountId($socialAccountId)->whereSocialAccountType($service)->first();
+					$socialAccountId = $user->getId();
+					$email = $user->getEmail();
+					$name = $user->getName();
+					$user = User::whereSocialAccountId($socialAccountId)->whereSocialAccountType($service)->first();
 
-        if (!$user) {
-            $user = User::create([
-                'name' => $name,
-                'email' => $email,
-                'social_account_id' => $socialAccountId,
-                'social_account_type' => $service,
-            ]);
-            RegisterController::create_new_user_note($user);
-        }
+					if (!$user) {
+							$user = User::create([
+									'name' => $name,
+									'email' => $email,
+									'social_account_id' => $socialAccountId,
+									'social_account_type' => $service,
+							]);
+							RegisterController::create_new_user_note($user);
+					}
 
-        //second parameter for remembering user
-        Auth::login($user, true);
+					//second parameter for remembering user
+					Auth::login($user, true);
 
-        return redirect()->route('home');
-    }
-}
+					return redirect()->route('home');
+			}
+	}
--- config/services.php
+++ config/services.php
@@ -1,61 +1,61 @@
-<?php
+	<?php
 
-return [
+	return [
 
-/*
-|--------------------------------------------------------------------------
-| Third Party Services
-|--------------------------------------------------------------------------
-|
-| This file is for storing the credentials for third party services such
-| as Stripe, Mailgun, SparkPost and others. This file provides a sane
-| default location for this type of information, allowing packages
-| to have a conventional place to find your various credentials.
-|
- */
+	/*
+	|--------------------------------------------------------------------------
+	| Third Party Services
+	|--------------------------------------------------------------------------
+	|
+	| This file is for storing the credentials for third party services such
+	| as Stripe, Mailgun, SparkPost and others. This file provides a sane
+	| default location for this type of information, allowing packages
+	| to have a conventional place to find your various credentials.
+	|
+	*/
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-    ],
+			'mailgun' => [
+					'domain' => env('MAILGUN_DOMAIN'),
+					'secret' => env('MAILGUN_SECRET'),
+					'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+			],
 
-    'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => env('SES_REGION', 'us-east-1'),
-    ],
+			'ses' => [
+					'key' => env('SES_KEY'),
+					'secret' => env('SES_SECRET'),
+					'region' => env('SES_REGION', 'us-east-1'),
+			],
 
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
-    ],
+			'sparkpost' => [
+					'secret' => env('SPARKPOST_SECRET'),
+			],
 
-    'stripe' => [
-        'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-        'webhook' => [
-            'secret' => env('STRIPE_WEBHOOK_SECRET'),
-            'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
-        ],
-    ],
+			'stripe' => [
+					'model' => App\User::class,
+					'key' => env('STRIPE_KEY'),
+					'secret' => env('STRIPE_SECRET'),
+					'webhook' => [
+							'secret' => env('STRIPE_WEBHOOK_SECRET'),
+							'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
+					],
+			],
 
-    'facebook' => [
-        'client_id' => env('FACEBOOK_CLIENT_ID'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/callback/facebook',
-    ],
+			'facebook' => [
+					'client_id' => env('FACEBOOK_CLIENT_ID'),
+					'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+					'redirect' => env('APP_URL') . '/login/callback/facebook',
+			],
 
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/callback/google',
-    ],
+			'google' => [
+					'client_id' => env('GOOGLE_CLIENT_ID'),
+					'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+					'redirect' => env('APP_URL') . '/login/callback/google',
+			],
 
-    'twitter' => [
-        'client_id' => env('TWITTER_CLIENT_ID'),
-        'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/callback/twitter',
-    ],
+			'twitter' => [
+					'client_id' => env('TWITTER_CLIENT_ID'),
+					'client_secret' => env('TWITTER_CLIENT_SECRET'),
+					'redirect' => env('APP_URL') . '/login/callback/twitter',
+			],
 
-];
+	];
