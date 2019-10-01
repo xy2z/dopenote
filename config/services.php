65ebe361d8b3d0fa478da4deca41616fb777f102
junaid ahmad <junaid.ahmad@synavos.com>
2019-10-01 15:36:53 +0500

    formatting issues

--- config/services.php
+++ config/services.php
@@ -2,17 +2,17 @@
 
 return [
 
-/*
-|--------------------------------------------------------------------------
-| Third Party Services
-|--------------------------------------------------------------------------
-|
-| This file is for storing the credentials for third party services such
-| as Stripe, Mailgun, SparkPost and others. This file provides a sane
-| default location for this type of information, allowing packages
-| to have a conventional place to find your various credentials.
-|
- */
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+     */
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
@@ -38,24 +38,6 @@ return [
             'secret' => env('STRIPE_WEBHOOK_SECRET'),
             'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
         ],
-    ],
-
-    'facebook' => [
-        'client_id' => env('FACEBOOK_CLIENT_ID'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/callback/facebook',
-    ],
-
-    'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/callback/google',
-    ],
-
-    'twitter' => [
-        'client_id' => env('TWITTER_CLIENT_ID'),
-        'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/callback/twitter',
     ],
 
 ];
