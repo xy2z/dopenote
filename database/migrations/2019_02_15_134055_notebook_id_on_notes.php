0a1b1fd44c1720c263c0c585ddf35b28ee50cd7a
Christian Leo-Pernold <christian.leo@akwien.at>
2019-10-02 15:14:07 +0200

    Fix coding style.

--- database/migrations/2019_02_15_134055_notebook_id_on_notes.php
+++ database/migrations/2019_02_15_134055_notebook_id_on_notes.php
@@ -4,27 +4,24 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class NotebookIdOnNotes extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('notes', function (Blueprint $table) {
-            $table->unsignedInteger('notebook_id')->nullable()->after('deleted_at');
-        });
-    }
+class NotebookIdOnNotes extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('notes', function (Blueprint $table) {
+			$table->unsignedInteger('notebook_id')->nullable()->after('deleted_at');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		//
+	}
 }
