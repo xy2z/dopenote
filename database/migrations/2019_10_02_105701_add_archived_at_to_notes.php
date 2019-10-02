418a3b09575f1ea56027204cd01963b4b8d425b3
Christian Leo-Pernold <christian.leo@akwien.at>
2019-10-02 14:53:16 +0200

    Fix identation. Add drop statement.

--- database/migrations/2019_10_02_105701_add_archived_at_to_notes.php
+++ database/migrations/2019_10_02_105701_add_archived_at_to_notes.php
@@ -4,29 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddArchivedAtToNotes extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('notes', function (Blueprint $table) {
-            $table->datetime('archived_at')->nullable();
-        });
-    }
+class AddArchivedAtToNotes extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('notes', function (Blueprint $table) {
+			$table->datetime('archived_at')->nullable();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('notes', function (Blueprint $table) {
-            //
-        });
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('notes', function (Blueprint $table) {
+			$table->dropColumn('archived_at');
+		});
+	}
 }
